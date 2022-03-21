d89bc3d97b3331dac82f2a53ce760842f6f9b94f
SharedPreferences <boothaganesh.jdev@gmail.com>
2022-03-21 15:16:09 +0530

    v1

---
+++ app/src/main/java/mb/ganesh/simpleforegroundapp/MainActivity.java
@@ -0,0 +1,40 @@
+package mb.ganesh.simpleforegroundapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import android.widget.EditText;
+
+import mb.ganesh.simplenotificationapp.R;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText editTextInput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        setContentView(R.layout.activity_main);
+
+        editTextInput = findViewById(R.id.edit_text_input);
+    }
+
+    public void startService(View v) {
+        String input = editTextInput.getText().toString();
+
+        Intent serviceIntent = new Intent(this, ExampleService.class);
+        serviceIntent.putExtra("inputExtra", input);
+
+        ContextCompat.startForegroundService(this, serviceIntent);
+    }
+
+    public void stopService(View v) {
+        Intent serviceIntent = new Intent(this, ExampleService.class);
+        stopService(serviceIntent);
+    }
+}
